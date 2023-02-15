84a8d06c29544c18c09a7304697e65ffa118869a
GerrysatriaH <gerrysatria10@gmail.com>
2023-02-15 22:02:54 +0700

    menambahkan file header dan navbar

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+// Function Component
+
+export default function navbar() {
+  return (
+    <div>
+        <h2>Ini Navbar</h2>
+    </div>
+  )
+}
+
+// Class Component
+
+// import React, { Component } from 'react'
+// export default class navbar extends Component {
+//   render() {
+//     return (
+//       <div>navbar</div>
+//     )
+//   }
+// }
+
+// Arrow Function
+// export default const navbar = () => {
+//   return (
+//     <div>navbar</div>
+//   )
+// }

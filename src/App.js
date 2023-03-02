b7ed8b9a2b16c44cbb19784af16539f64a35c8f3
GerrysatriaH <gerrysatria10@gmail.com>
2023-03-02 20:22:26 +0700

    fix error on react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import Header from './Header';
 import Navbar from './Navbar';
 
 function App() {
-  let user = "Gerry";
   return (
     <div className="App">
       <Navbar />
       <Header />
-      <h2>Halo, { user }</h2>
+      <h2>Halo, Gerry</h2>
       <button>Login</button>
     </div>
   );

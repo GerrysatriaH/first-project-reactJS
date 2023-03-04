9f106fe5fe6b1a7776c53dc82eea2771a4d07555
GerrysatriaH <gerrysatria10@gmail.com>
2023-03-04 21:49:04 +0700

    navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,11 +5,11 @@ import './App.css';
 export default function navbar() {
   return (
     <div className='navbar'>
-        <h2>NationalLibrary</h2>
-        <ul>
-          <li>Home</li>
-          <li>Booklist</li>
-          <li>Bookshelf</li>
+        <h2 className='title'>NationalLibrary</h2>
+        <ul className='list-menu'>
+          <li className='menu'>Home</li>
+          <li className='menu'>Booklist</li>
+          <li className='menu'>Bookshelf</li>
         </ul>
     </div>
   )

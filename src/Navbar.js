15ef601b14c4b96bbbe68fe6b82481d244d77f1a
GerrysatriaH <gerrysatria10@gmail.com>
2023-03-07 21:26:15 +0700

    edit navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
 import './App.css';
+import logo from './img/logo_ipusnas.png';
+import search from './img/search_icon.png';
 
 export default function navbar() {
   return (
     <div className='navbar'>
-        <h2 className='title'>NationalLibrary</h2>
+        <img src={logo} alt="logo-nav" className='logo'/>
         <ul className='list-menu'>
           <li className='menu'>Home</li>
-          <li className='menu'>Booklist</li>
+          <li className='menu'>Library</li>
           <li className='menu'>Bookshelf</li>
+          <li className='menu'>Feeds</li>
         </ul>
+        <form>
+          <input type="text"></input>
+          <button>
+            <img src={search} alt='search'></img>
+          </button>
+        </form>
     </div>
   )
 }

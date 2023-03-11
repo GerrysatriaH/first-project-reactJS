a7388355627bff607d99c174dabc48b6a4e2dd99
GerrysatriaH <gerrysatria10@gmail.com>
2023-03-11 22:21:44 +0700

    edit navbar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -4,6 +4,8 @@ import '../App.css';
 import './Navbar.css';
 import logo from '../../img/logo_ipusnas.png';
 import search from '../../img/search_icon.png';
+import notification from '../../img/notif_icon.png';
+import user_icon from '../../img/user_icon.png';
 
 export default function navbar() {
   return (
@@ -21,6 +23,12 @@ export default function navbar() {
             <img src={search} alt='search'></img>
           </button>
         </form>
+        <button>
+          <img src={notification} alt='notif_icon'></img>
+        </button>
+        <button>
+          <img src={user_icon} alt='user_icon'></img>
+        </button>
     </div>
   )
 }
